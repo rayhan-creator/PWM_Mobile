326196cf58b724edfea9efa4f53afafe9a7234cf
rayhan-creator <mhdrayhanadr@gmail.com>
2025-07-29 07:35:22 +0700

    Hari Pertama

---
+++ day 1/day1.dart
@@ -0,0 +1,12 @@
+void main() {
+  var hari = "selasa";
+  int harga = 30000;
+  if (hari == "jumat") {
+    harga = 20000;
+  } else if (hari == "sabtu" || hari == "ahad") {
+    harga = 40000;
+  } else {
+    harga = 30000;
+  }
+  print('Harga tiketnya adalah : Rp${harga}');
+}
